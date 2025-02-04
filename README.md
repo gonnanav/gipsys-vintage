8a51dfe49fea118c6c6cd8d46a6fd3fa408f36dd
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-02-04 17:22:03 +0200

    build: create nextjs project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gipsy's Vintage Web App
+
+This project contains the full-stack development for Gipsy's Vintage, an e-commerce web app selling vintage clothing items for women for a local Israeli audience.
---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Gipsy's Vintage",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="he">
+      <body>{children}</body>
+    </html>
+  );
+}
