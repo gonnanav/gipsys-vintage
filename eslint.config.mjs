852cea3ad1b14d8277772fe122ba31b181a44197
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-02-05 20:04:29 +0200

    build(lint): ignore all files except app and lib dirs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   eslintConfigPrettier,
+  {
+    ignores: ['*', '!app', '!lib'],
+  },
 ];
 
 export default eslintConfig;
