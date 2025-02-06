c8170df879f55baa11a92779ac13d8c1afc788b6
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-02-06 18:22:55 +0200

    test(e2e): write failing products list test

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,61 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      const customerKey = config.env['WC_CUSTOMER_KEY'];
+      if (!customerKey) {
+        throw new Error('Missing WooCommerce customer key');
+      }
+
+      const customerSecret = config.env['WC_CUSTOMER_SECRET'];
+      if (!customerSecret) {
+        throw new Error('Missing WooCommerce customer secret');
+      }
+
+      const credentials = Buffer.from(
+        `${customerKey}:${customerSecret}`,
+      ).toString('base64');
+
+      const headers = {
+        Authorization: `Basic ${credentials}`,
+        'Content-Type': 'application/json',
+      };
+
+      on('task', {
+        'reset:products': async () => {
+          const productsResponse = await fetch(
+            'https://gipsys-vintage.local/wp-json/wc/v3/products',
+            {
+              headers,
+            },
+          );
+          const json = await productsResponse.json();
+          const productIds = json.map((product: any) => product.id);
+
+          const deleteRequest = await fetch(
+            'https://gipsys-vintage.local/wp-json/wc/v3/products/batch',
+            {
+              method: 'POST',
+              headers,
+              body: JSON.stringify({ delete: productIds }),
+            },
+          );
+          const deleteJson = await deleteRequest.json();
+
+          return null;
+        },
+        'seed:products': async (products) => {
+          const createResponse = await fetch(
+            'https://gipsys-vintage.local/wp-json/wc/v3/products/batch',
+            {
+              method: 'POST',
+              headers,
+              body: JSON.stringify({ create: products }),
+            },
+          );
+
+          return null;
+        },
+      });
     },
     baseUrl: process.env.E2E_BASE_URL || 'http://localhost:3000',
   },
--- cypress/e2e/shop-page.cy.ts
+++ cypress/e2e/shop-page.cy.ts
@@ -1,5 +1,16 @@
+const products = [{ name: 'product1' }, { name: 'product2' }];
+
 describe('Shop Page', () => {
+  beforeEach(() => {
+    cy.task('reset:products');
+    cy.task('seed:products', products);
+  });
+
   it('displays the list of products', () => {
     cy.visit('/shop');
+
+    products.forEach((product) => {
+      cy.contains(product.name).should('be.visible');
+    });
   });
 });
