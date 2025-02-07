e17792d20c0d11cf2f4ec3d5fd5fef5713e7225e
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-02-07 15:21:05 +0200

    feat(products): display products on shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,3 +1,37 @@
-export default function Page() {
-  return <></>;
+const customerKey = process.env['WC_CUSTOMER_KEY'];
+if (!customerKey) {
+  throw new Error('Missing WooCommerce customer key');
+}
+
+const customerSecret = process.env['WC_CUSTOMER_SECRET'];
+if (!customerSecret) {
+  throw new Error('Missing WooCommerce customer secret');
+}
+
+const credentials = Buffer.from(`${customerKey}:${customerSecret}`).toString(
+  'base64',
+);
+
+const headers = {
+  Authorization: `Basic ${credentials}`,
+  'Content-Type': 'application/json',
+};
+
+export default async function Page() {
+  const productsResponse = await fetch(
+    'https://gipsys-vintage.local/wp-json/wc/v3/products',
+    {
+      headers,
+      cache: 'no-store',
+    },
+  );
+  const products = await productsResponse.json();
+
+  return (
+    <>
+      {products.map((product: { name: string; id: number }) => (
+        <div key={product.id}>{product.name}</div>
+      ))}
+    </>
+  );
 }
