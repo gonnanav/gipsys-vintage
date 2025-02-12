0d034fe208f4b8dd9c2780de37ca6685070c2682
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-02-12 04:30:05 +0200

    build(lint): add cypress plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginCypress from 'eslint-plugin-cypress/flat';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,6 +13,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  pluginCypress.configs.recommended,
   eslintConfigPrettier,
   {
     ignores: ['*', '!src', '!cypress'],
