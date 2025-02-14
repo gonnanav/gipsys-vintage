2b412f41c85732b14723c214ed70f5cfe9b4bddc
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-02-14 20:10:04 +0200

    build: install storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import pluginCypress from 'eslint-plugin-cypress/flat';
+import storybook from 'eslint-plugin-storybook';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,11 +13,12 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  ...storybook.configs['flat/recommended'],
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
   pluginCypress.configs.recommended,
   eslintConfigPrettier,
   {
-    ignores: ['*', '!src', '!e2e'],
+    ignores: ['*', '!src', '!e2e', '!.storybook'],
   },
 ];
 
