bf5d31cd2c76acd4c0a92564d67eecff82c8cfe4
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-02-18 17:49:26 +0200

    test(e2e/shop): check navigation to product page

--- e2e/cypress/e2e/shop-page.cy.ts
+++ e2e/cypress/e2e/shop-page.cy.ts
@@ -1,7 +1,7 @@
 import { NewProduct, Product } from '@/application';
 
 describe('Shop Page', () => {
-  it('displays the list of products', () => {
+  it('displays list of products and navigates to product page when clicking it', () => {
     const newProducts: NewProduct[] = [
       { name: 'product1', price: '100' },
       { name: 'product2', price: '50' },
@@ -13,10 +13,16 @@ describe('Shop Page', () => {
     cy.get('h1').contains('חנות').should('be.visible');
 
     cy.get<Product[]>('@products').then((products) => {
+      // Check that all products are displayed
       products.forEach((product) => {
         cy.contains(product.name).should('be.visible');
         cy.contains(product.price).should('be.visible');
       });
+
+      // Click the first product and check that it navigates to its page
+      const firstProduct = products[0];
+      cy.contains(firstProduct.name).click();
+      cy.location('pathname').should('eq', `/shop/${firstProduct.slug}`);
     });
   });
 });
