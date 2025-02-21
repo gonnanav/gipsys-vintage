1cac7541a75dc5adfe0ad58865df3613ddee894a
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-02-21 22:41:52 +0200

    feat(product): add ui styling for product page

--- src/app/product/[slug]/components/product-details.tsx
+++ src/app/product/[slug]/components/product-details.tsx
@@ -1,4 +1,6 @@
 import { Product } from '@/application';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 type ProductDetailsProps = {
   product: Product;
@@ -8,10 +10,24 @@ export function ProductDetails({ product }: ProductDetailsProps) {
   const { name, price, description } = product;
 
   return (
-    <>
-      <h1>{name}</h1>
-      <div>{`₪${price}`}</div>
-      {description && <div>{description}</div>}
-    </>
+    <Stack spacing={2}>
+      <Stack spacing={1}>
+        <Typography variant="h1">{name}</Typography>
+        <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+          ₪{price}
+        </Typography>
+      </Stack>
+      {description && (
+        <Typography
+          variant="body1"
+          color="text.secondary"
+          sx={{
+            lineHeight: 1.8,
+          }}
+        >
+          {description}
+        </Typography>
+      )}
+    </Stack>
   );
 }
