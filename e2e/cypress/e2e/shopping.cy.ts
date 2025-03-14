6d2992b5278ab342d433e7522c6a139120de69de
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-03-14 17:33:04 +0200

    refactor(e2e): rename cart item functions and update test descriptions

--- e2e/cypress/e2e/shopping.cy.ts
+++ e2e/cypress/e2e/shopping.cy.ts
@@ -2,38 +2,38 @@ import { Product } from '@/core/product';
 import { shoppingCart, productPage } from '../support/page-objects';
 import { testData } from '../support/helpers';
 
-const sampleProducts = testData.getSampleProducts().slice(0, 2);
-
 describe('Shopping Journey', () => {
-  it('updates items in the shopping cart', () => {
+  it('adds and removes items from the shopping cart', () => {
+    const sampleProducts = testData.getSampleProducts().slice(0, 2);
+
     testData.seedProducts(sampleProducts).then((products) => {
       cy.visit('/');
       shoppingCart.open();
       verifyCartIsEmpty();
 
-      addProductToCart(products[0]);
+      addItemToCart(products[0]);
       shoppingCart.open();
       verifyItemsInCart([products[0]]);
 
-      addProductToCart(products[1]);
+      addItemToCart(products[1]);
       shoppingCart.open();
       verifyItemsInCart([products[0], products[1]]);
 
-      removeProductFromCart(products[0]);
+      removeItemFromCart(products[0]);
       verifyItemsInCart([products[1]]);
 
-      removeProductFromCart(products[1]);
+      removeItemFromCart(products[1]);
       verifyCartIsEmpty();
     });
   });
 });
 
-function addProductToCart(product: Product): void {
+function addItemToCart(product: Product): void {
   productPage.visit(product);
   productPage.addToCart();
 }
 
-function removeProductFromCart(product: Product): void {
+function removeItemFromCart(product: Product): void {
   shoppingCart.removeItem(product);
 }
 
