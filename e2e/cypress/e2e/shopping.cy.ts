d278200b2ff8db88609935cb69d312b2086b56a6
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-03-19 00:35:07 +0200

    test(e2e): move shop tests inside shopping journey

--- e2e/cypress/e2e/shopping.cy.ts
+++ e2e/cypress/e2e/shopping.cy.ts
@@ -1,12 +1,30 @@
-import { Product } from '@/core/product';
+import { Product, ProductCreate } from '@/core/product';
+import { Category } from '@/core/category';
 import { header, cart, shopPage, productPage, data } from '../support/helpers';
+import { createProduct } from '../support/helpers/data';
 
 describe('Shopping Journey', () => {
+  let shirtsCategory: Category;
+  let pantsCategory: Category;
+  let whiteTShirt: Product;
+  let blueJeans: Product;
+  let blackPants: Product;
   let products: Product[] = [];
 
   before(() => {
-    data.seedProducts(data.sampleProducts).then((ps) => {
-      products = ps;
+    data.seedCategories([data.shirtsCategory, data.pantsCategory]).then((categories) => {
+      [shirtsCategory, pantsCategory] = categories;
+
+      const sampleProducts: ProductCreate[] = [
+        createProduct('White T-Shirt', shirtsCategory),
+        createProduct('Blue Jeans', pantsCategory),
+        createProduct('Black Pants', pantsCategory),
+      ];
+
+      data.seedProducts(sampleProducts).then((ps) => {
+        products = ps;
+        [whiteTShirt, blueJeans, blackPants] = ps;
+      });
     });
   });
 
@@ -36,6 +54,21 @@ describe('Shopping Journey', () => {
       removeItemFromCart(products[0]);
       verifyThatCartIsEmpty();
     });
+  });
+
+  it('displays all products and navigates to product page when clicking product', () => {
+    visitShopPage();
+
+    verifyThatShopTitleIs('חנות');
+    verifyThatProductsInShopAre(products);
+  });
+
+  it('displays products filtered by category when visiting category page', () => {
+    visitCategory(pantsCategory);
+
+    verifyThatShopTitleIs(pantsCategory.name);
+    verifyThatProductsInShopAre([blueJeans, blackPants]);
+    verifyThatProductIsNotInShop(whiteTShirt);
   });
 });
 
@@ -84,4 +117,28 @@ function verifyThatItemsInCartAre(expectedItems: Product[]): void {
   expectedItems.forEach(({ name }, index) => {
     cart.getItems().eq(index).contains(name).should('be.visible');
   });
+}
+
+function visitCategory(category: Category): void {
+  shopPage.visitCategory(category);
+}
+
+function verifyThatShopTitleIs(title: string): void {
+  cy.getPageHeading(title).should('be.visible');
+}
+
+function verifyThatProductsInShopAre(products: Product[]): void {
+  shopPage.getProducts().should('have.length', products.length);
+
+  products.forEach((product) => {
+    shopPage.getProduct(product.name).within(() => {
+      cy.contains(product.name).should('be.visible');
+      cy.contains(product.price).should('be.visible');
+      cy.getByTestId('product-card-image').should('be.visible');
+    });
+  });
+}
+
+function verifyThatProductIsNotInShop(product: Product): void {
+  shopPage.getProduct(product.name).should('not.exist');
 }
