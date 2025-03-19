6c6d21f0f2483c80f4b78d9df0348cd81c2d29db
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-03-19 23:43:48 +0200

    feat(nav): add stories for nav drawer ui

---
+++ src/ui/components/nav-drawer/nav-drawer.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { NavDrawer } from './nav-drawer';
+import { StoreProvider } from '@/store';
+
+const meta: Meta<typeof NavDrawer> = {
+  title: 'Global/NavDrawer',
+  component: NavDrawer,
+  decorators: [
+    (Story) => (
+      <StoreProvider initialState={{ isNavDrawerOpen: true }}>
+        <Story />
+      </StoreProvider>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof NavDrawer>;
+
+export const NavMenu: Story = {};
