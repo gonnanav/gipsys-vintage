0345ce06224d6b8b25dc0083b97712e6fff280b4
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-03-19 21:27:22 +0200

    feat(nav): add shop page link

--- src/ui/components/nav-drawer/nav-drawer.test.tsx
+++ src/ui/components/nav-drawer/nav-drawer.test.tsx
@@ -1,23 +1,61 @@
+import { render, screen, within } from '@testing-library/react';
 import { StoreProvider } from '@/store';
-import { render, screen } from '@testing-library/react';
 import { NavDrawer } from './nav-drawer';
 
-it('does not render the navigation drawer when closed', () => {
-  render(
-    <StoreProvider initialState={{ isNavDrawerOpen: false }}>
+it('renders no content when the drawer is closed', () => {
+  const { container } = renderNavDrawer({ isOpen: false });
+
+  expect(container).toBeEmptyDOMElement();
+});
+
+it('renders the drawer modal', () => {
+  renderNavDrawer();
+
+  expect(getNavigationDrawer()).toHaveAttribute('aria-modal', 'true');
+});
+
+it('renders the navigation menu', () => {
+  renderNavDrawer();
+
+  expect(getNavigationMenu()).toBeInTheDocument();
+});
+
+it('renders the navigation list', () => {
+  renderNavDrawer();
+
+  expect(getNavigationList()).toBeInTheDocument();
+});
+
+it('renders a link to the shop page', async () => {
+  renderNavDrawer();
+
+  expect(getShopLink()).toHaveAttribute('href', '/shop');
+});
+
+interface RenderNavDrawerProps {
+  isOpen?: boolean;
+}
+
+function renderNavDrawer({ isOpen = true }: RenderNavDrawerProps = {}) {
+  return render(
+    <StoreProvider initialState={{ isNavDrawerOpen: isOpen }}>
       <NavDrawer />
     </StoreProvider>,
   );
+}
 
-  expect(screen.queryByRole('navigation')).not.toBeInTheDocument();
-});
+function getNavigationDrawer() {
+  return screen.getByRole('dialog');
+}
 
-it('renders the navigation drawer when open', () => {
-  render(
-    <StoreProvider initialState={{ isNavDrawerOpen: true }}>
-      <NavDrawer />
-    </StoreProvider>,
-  );
+function getNavigationMenu() {
+  return within(getNavigationDrawer()).getByRole('navigation');
+}
 
-  expect(screen.getByRole('navigation')).toBeInTheDocument();
-});
+function getNavigationList() {
+  return within(getNavigationMenu()).getByRole('list');
+}
+
+function getShopLink() {
+  return within(getNavigationList()).getByRole('link', { name: 'חנות' });
+}
--- src/ui/components/nav-drawer/nav-drawer.tsx
+++ src/ui/components/nav-drawer/nav-drawer.tsx
@@ -1,9 +1,24 @@
 'use client';
 
+import Link from 'next/link';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 import { useNavDrawer } from '@/store';
 
 export function NavDrawer() {
   const { isOpen } = useNavDrawer();
 
-  return isOpen ? <nav /> : null;
+  return (
+    <Drawer role="dialog" aria-modal="true" open={isOpen}>
+      <nav>
+        <div>תפריט הניווט</div>
+        <List>
+          <ListItem>
+            <Link href="/shop">חנות</Link>
+          </ListItem>
+        </List>
+      </nav>
+    </Drawer>
+  );
 }
