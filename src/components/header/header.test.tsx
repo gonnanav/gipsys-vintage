3f21470875b5fbf1310451acce38a5c334d0b14e
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-03-22 17:45:50 +0200

    refactor(header): simplify test and make it more readable

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,48 +1,68 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { Header } from './header';
 import { StoreProvider } from '@/store';
+import { Header } from './header';
 import { NavDrawer } from '../nav-drawer/nav-drawer';
+import { CartDrawer } from '../cart-drawer/cart-drawer';
 
-it('renders a banner', () => {
-  renderAppHeader();
+it('renders the logo', () => {
+  render(<Header />, { wrapper: StoreProvider });
 
-  expect(screen.getByRole('banner')).toBeInTheDocument();
+  expect(getLogo()).toBeInTheDocument();
 });
 
-it('renders the app header logo with the logo image', () => {
-  renderAppHeader();
+it('opens the navigation drawer', async () => {
+  const { openNavDrawer } = renderHeaderWithDrawers();
 
-  expect(screen.getByRole('img', { name: "Gipsy's Vintage Logo" })).toBeInTheDocument();
+  await openNavDrawer();
+
+  expect(getNavDrawer()).toBeInTheDocument();
 });
 
-it('renders the shopping cart button', () => {
-  renderAppHeader();
+it('opens the shopping cart drawer', async () => {
+  const { openCartDrawer } = renderHeaderWithDrawers();
 
-  expect(screen.getByRole('button', { name: 'פתחי את סל הקניות' })).toBeInTheDocument();
+  await openCartDrawer();
+
+  expect(getCartDrawer()).toBeInTheDocument();
 });
 
-it('opens the navigation drawer when the navigation button is clicked', async () => {
-  const { user } = renderAppHeaderWithNavDrawer();
-
-  await user.click(screen.getByRole('button', { name: 'פתחי את תפריט הניווט' }));
-
-  expect(screen.getByRole('navigation')).toBeInTheDocument();
-});
-
-function renderAppHeader() {
-  return render(<Header />, { wrapper: StoreProvider });
-}
-
-function renderAppHeaderWithNavDrawer() {
+function renderHeaderWithDrawers() {
   const user = userEvent.setup();
+  const openNavDrawer = () => user.click(getOpenNavigationButton());
+  const openCartDrawer = () => user.click(getOpenCartButton());
 
   render(
     <StoreProvider>
       <Header />
       <NavDrawer />
+      <CartDrawer />
     </StoreProvider>,
   );
 
-  return { user };
+  return { user, openNavDrawer, openCartDrawer };
+}
+
+function getHeader() {
+  return screen.getByRole('banner');
+}
+
+function getLogo() {
+  return within(getHeader()).getByRole('img', { name: "Gipsy's Vintage Logo" });
+}
+
+function getOpenNavigationButton() {
+  return within(getHeader()).getByRole('button', { name: 'פתחי את תפריט הניווט' });
+}
+
+function getOpenCartButton() {
+  return within(getHeader()).getByRole('button', { name: 'פתחי את סל הקניות' });
+}
+
+function getNavDrawer() {
+  return screen.getByRole('dialog', { name: 'תפריט הניווט' });
+}
+
+function getCartDrawer() {
+  return screen.getByRole('dialog', { name: 'סל הקניות' });
 }
