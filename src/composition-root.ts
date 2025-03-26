2a38cbfa82245262bd3141b8082ac51f7e614aac
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-03-26 17:39:44 +0200

    refactor(woocommerce): delete unnecessary interfaces and test

--- src/composition-root.ts
+++ src/composition-root.ts
@@ -1,12 +1,12 @@
 import { Application } from './core/application';
 import { WooCommerceAdapter } from './woocommerce/adapter';
-import { WooCommerceClient } from './woocommerce/client';
+import { WooCommerceApi } from './woocommerce/api';
 
 export function createApplication(): Application {
   const wcUrl = getRequiredEnv('WOOCOMMERCE_URL');
   const wcCustomerKey = getRequiredEnv('WOOCOMMERCE_CUSTOMER_KEY');
   const wcCustomerSecret = getRequiredEnv('WOOCOMMERCE_CUSTOMER_SECRET');
-  const wcClient = new WooCommerceClient(wcUrl, wcCustomerKey, wcCustomerSecret);
+  const wcClient = new WooCommerceApi(wcUrl, wcCustomerKey, wcCustomerSecret);
 
   return new WooCommerceAdapter(wcClient);
 }
