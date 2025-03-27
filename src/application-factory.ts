5567c72050031b8ed7e4a8b4cb869f891413ce10
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-03-27 17:30:20 +0200

    fix: update messages when parsing environment

--- src/application-factory.ts
+++ src/application-factory.ts
@@ -4,9 +4,15 @@ import { WooCommerceAdapter } from './woocommerce/adapter';
 import { WooCommerceApi } from './woocommerce/api';
 
 const envSchema = z.object({
-  WOOCOMMERCE_URL: z.string().min(1, 'WooCommerce URL is required').url('Invalid WooCommerce URL'),
-  WOOCOMMERCE_CUSTOMER_KEY: z.string().min(1, 'WooCommerce Customer Key is required'),
-  WOOCOMMERCE_CUSTOMER_SECRET: z.string().min(1, 'WooCommerce Customer Secret is required'),
+  WOOCOMMERCE_URL: z
+    .string({ message: 'WooCommerce URL is required' })
+    .url({ message: 'Invalid WooCommerce URL' }),
+  WOOCOMMERCE_CUSTOMER_KEY: z
+    .string({ message: 'WooCommerce customer key is required' })
+    .min(1, { message: 'Empty WooCommerce customer key' }),
+  WOOCOMMERCE_CUSTOMER_SECRET: z
+    .string({ message: 'WooCommerce customer secret is required' })
+    .min(1, { message: 'Empty WooCommerce customer secret' }),
 });
 
 /**
