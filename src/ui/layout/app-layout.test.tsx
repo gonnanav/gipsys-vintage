c6c46400d2c6660af9c79fd46689cef449139f97
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-04-09 21:52:57 +0300

    refactor(app-layout): remove unused user interaction tests and related functions

--- src/ui/layout/app-layout.test.tsx
+++ src/ui/layout/app-layout.test.tsx
@@ -1,5 +1,4 @@
 import { render, screen, within } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { StoreProvider } from '@/ui/store';
 import { AppLayout, AppLayoutProps } from './app-layout';
 
@@ -15,31 +14,8 @@ it('renders the main content', () => {
   expect(getMain()).toHaveTextContent('Main Content');
 });
 
-it('opens the navigation drawer', async () => {
-  const { openNavDrawer } = renderAppLayoutWithUserActions();
-
-  await openNavDrawer();
-
-  expect(getNavDrawer()).toBeInTheDocument();
-});
-
 function renderAppLayout(props: Partial<AppLayoutProps> = {}) {
   return render(<AppLayout {...props}>{props.children}</AppLayout>, { wrapper: StoreProvider });
-}
-
-function renderAppLayoutWithUserActions(props: Partial<AppLayoutProps> = {}) {
-  const userActions = setupUserActions();
-  const renderResult = renderAppLayout(props);
-
-  return { ...renderResult, ...userActions };
-}
-
-function setupUserActions() {
-  const user = userEvent.setup();
-  const openNavDrawer = () => user.click(getOpenNavigationButton());
-  const openCartDrawer = () => user.click(getOpenCartButton());
-
-  return { openNavDrawer, openCartDrawer };
 }
 
 function getHeader() {
@@ -52,16 +28,4 @@ function getHeaderLogo() {
 
 function getMain() {
   return screen.getByRole('main');
-}
-
-function getOpenNavigationButton() {
-  return within(getHeader()).getByRole('button', { name: 'פתחי את תפריט הניווט' });
-}
-
-function getOpenCartButton() {
-  return within(getHeader()).getByRole('button', { name: 'פתחי את סל הקניות' });
-}
-
-function getNavDrawer() {
-  return screen.getByRole('dialog', { name: 'תפריט הניווט' });
 }
