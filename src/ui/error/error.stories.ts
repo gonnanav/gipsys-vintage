7cd0a97a42e41ca26858a035a17b78d34e6a666b
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-04-10 16:38:45 +0300

    refactor(error): rename files

--- src/ui/error/error.stories.ts
+++ src/ui/error/error.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import { ErrorPage } from './error-page';
+import { ErrorPage } from './error';
 
 const meta = {
   title: 'Error Page',
