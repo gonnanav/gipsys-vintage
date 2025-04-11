aa88c0b50a73c8abcd9976a45f919ddae5c8ec7f
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-04-11 23:24:59 +0300

    feat(product): add createProduct function

--- src/core/product.ts
+++ src/core/product.ts
@@ -3,11 +3,27 @@ export interface Product {
   name: string;
   slug: string;
   price: string;
+  formattedPrice?: string;
   description: string;
   images: ProductImage[];
+  mainImage?: ProductImage;
+}
+
+export interface ProductInput {
+  id: number;
+  name: string;
+  slug: string;
+  price: string;
+  description?: string;
+  images?: ProductImageInput[];
 }
 
 export interface ProductImage {
+  src: string;
+  alt: string;
+}
+
+export interface ProductImageInput {
   src: string;
   alt: string;
 }
@@ -24,3 +40,18 @@ export const placeholderImage: ProductImage = {
   src: '/images/product-placeholder.webp',
   alt: 'אין תמונת מוצר',
 };
+
+export function createProduct(input: ProductInput): Product {
+  const mainImage = getMainImage(input.images);
+  const formattedPrice = formatPrice(input.price);
+
+  return { description: '', images: [], ...input, mainImage, formattedPrice };
+}
+
+function getMainImage(images?: ProductImageInput[]): ProductImage {
+  return images?.[0] ?? placeholderImage;
+}
+
+function formatPrice(price: string): string {
+  return `₪${price}`;
+}
