8122375b8d6d6200a70340dcfad47cd611e7cdb9
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-04-14 17:43:07 +0300

    fix(env): remove return type

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,23 +1,17 @@
 import { z } from 'zod';
 
-export interface EnvConfig {
-  WOOCOMMERCE_URL: string;
-  WOOCOMMERCE_CUSTOMER_KEY: string;
-  WOOCOMMERCE_CUSTOMER_SECRET: string;
-}
+const schema = z.object({
+  WOOCOMMERCE_URL: z
+    .string({ message: 'WooCommerce URL is required' })
+    .url({ message: 'Invalid WooCommerce URL' }),
+  WOOCOMMERCE_CUSTOMER_KEY: z
+    .string({ message: 'WooCommerce customer key is required' })
+    .min(1, { message: 'Empty WooCommerce customer key' }),
+  WOOCOMMERCE_CUSTOMER_SECRET: z
+    .string({ message: 'WooCommerce customer secret is required' })
+    .min(1, { message: 'Empty WooCommerce customer secret' }),
+});
 
-export function parseEnv(): EnvConfig {
-  const schema = z.object({
-    WOOCOMMERCE_URL: z
-      .string({ message: 'WooCommerce URL is required' })
-      .url({ message: 'Invalid WooCommerce URL' }),
-    WOOCOMMERCE_CUSTOMER_KEY: z
-      .string({ message: 'WooCommerce customer key is required' })
-      .min(1, { message: 'Empty WooCommerce customer key' }),
-    WOOCOMMERCE_CUSTOMER_SECRET: z
-      .string({ message: 'WooCommerce customer secret is required' })
-      .min(1, { message: 'Empty WooCommerce customer secret' }),
-  });
-
+export function parseEnv() {
   return schema.parse(process.env);
 }
