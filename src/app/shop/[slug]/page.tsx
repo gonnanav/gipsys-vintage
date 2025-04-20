ae71b522d0607cbd69367de1d103738413a5e33f
Gonn Anav <45741938+gonnanav@users.noreply.github.com>
2025-04-20 18:40:57 +0300

    refactor(shop): parse categories directly in shop category controller

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -1,6 +1,7 @@
-import { getCategoryWithProducts } from '@/services';
+import { wcService, fromWooCommerceProduct, parseProducts } from '@/services';
 import { ShopPage } from '@/components/shop';
 import { notFound } from 'next/navigation';
+import { parseCategories } from '@/services/woocommerce/category';
 
 interface PageProps {
   params: Promise<{ slug: string }>;
@@ -8,8 +9,16 @@ interface PageProps {
 
 export default async function Page({ params }: PageProps) {
   const slug = (await params).slug;
-  const category = await getCategoryWithProducts(slug);
-  if (!category) notFound();
 
-  return <ShopPage title={category.name} products={category.products} />;
+  const rawCategories = await wcService.get('products/categories', { slug });
+  const wcCategories = parseCategories(rawCategories);
+  const wcCategory = wcCategories[0];
+
+  if (!wcCategory) notFound();
+
+  const rawProducts = await wcService.get('products', { category: wcCategory.id.toString() });
+  const wcProducts = parseProducts(rawProducts);
+  const products = wcProducts.map(fromWooCommerceProduct);
+
+  return <ShopPage title={wcCategory.name} products={products} />;
 }
